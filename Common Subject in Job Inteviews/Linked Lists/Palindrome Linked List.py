b41a4865ff35fda0200311f0001e87f8dbb81115
Itamar Shechter <84578656+itamar-sh@users.noreply.github.com>
2022-08-24 13:03:04 +0300

    Palindrome Linked List - advanced space

--- Common Subject in Job Inteviews/Linked Lists/Palindrome Linked List.py
+++ Common Subject in Job Inteviews/Linked Lists/Palindrome Linked List.py
@@ -6,7 +6,7 @@ class Node:
 
 class Solution:
     def Palindrome_Linked_List(self, head):
-        if not head:
+        if not head or not head.next:
             return True
         end1, head2 = self._reverse_half_list(head)
         p1 = head
@@ -25,12 +25,13 @@ class Solution:
     def _reverse_half_list(self, head):
         slow = head
         fast = head
+        prev = slow
         while fast and fast.next:
+            prev = slow
             slow = slow.next
             fast = fast.next.next
-        head2 = slow.next
-        slow.next = None
-        return slow, self._reverse_list(head2)
+        prev.next = None
+        return prev, self._reverse_list(slow)
 
     def _reverse_list(self, head):
         head2 = None
@@ -68,9 +69,13 @@ if __name__ == '__main__':
     head.next.next = Node(3)
     assert (not solver.Palindrome_Linked_List(head))
     printList(head)
-    head = Node(2)
-    head.next = Node(6)
-    head.next.next = Node(6)
-    head.next.next.next = Node(3)
+    head = Node(1)
+    head.next = Node(1)
+    head.next.next = Node(2)
+    head.next.next.next = Node(1)
+    assert (not solver.Palindrome_Linked_List(head))
+    printList(head)
+    head = Node(1)
+    head.next = Node(2)
     assert (not solver.Palindrome_Linked_List(head))
     printList(head)
