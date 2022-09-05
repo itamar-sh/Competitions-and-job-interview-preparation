75e88a4363d2de6a0418944dc3c1c159836a8bcf
Itamar Shechter <84578656+itamar-sh@users.noreply.github.com>
2022-09-05 12:06:28 +0300

    isinstance

--- OOP Templates/Python OOP Templates/Built in functions.py
+++ OOP Templates/Python OOP Templates/Built in functions.py
@@ -1,4 +1,15 @@
-# isinstance()
+# isinstance(object, classinfo) - return True if object is from type classinfo even is from inherited class.
+from typing import List
+
+print(isinstance([1,2,3], list))  # True
+# print(isinstance([1,2,3], List[int]))  # not working
+print(isinstance([1,2,3], (int, list)))  # classsinfo can be tuple of classes name # True
+class Foo:
+    a = 5
+fooInstance = Foo()
+print(isinstance(fooInstance, Foo))  # True
+print(isinstance(fooInstance, (list, tuple)))  # False
+print(isinstance(fooInstance, (list, tuple, Foo)))  # True
 
 # issubclass()
 
