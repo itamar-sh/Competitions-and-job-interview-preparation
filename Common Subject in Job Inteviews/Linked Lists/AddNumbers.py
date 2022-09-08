61fe4fd93cc15f599820db0f80f6dea640067a1c
Itamar Shechter <84578656+itamar-sh@users.noreply.github.com>
2022-09-08 12:27:45 +0300

    AddNumbers2 - Medium - 455

--- Common Subject in Job Inteviews/Linked Lists/AddNumbers.py
+++ Common Subject in Job Inteviews/Linked Lists/AddNumbers.py
@@ -3,12 +3,14 @@ class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
-"""
-You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
 
-You may assume the two numbers do not contain any leading zero, except the number 0 itself
-"""
 class Solution:
+    """
+    You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order,
+    and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+    You may assume the two numbers do not contain any leading zero, except the number 0 itself
+    """
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         head = ListNode()
         cur = head
@@ -31,3 +33,31 @@ class Solution:
             cur.next = ListNode(remainder)
             cur = cur.next
         return head.next
+
+    """
+    You are given two non-empty linked lists representing two non-negative integers. The most significant digit comes 
+    first and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+    You may assume the two numbers do not contain any leading zero, except the number 0 itsel
+    """
+    def addTwoNumbers2(self, l1, l2):
+        s1 = 0
+        s2 = 0
+        while l1:
+            s1 *= 10
+            s1 += l1.val
+            l1 = l1.next
+        while l2:
+            s2 *= 10
+            s2 += l2.val
+            l2 = l2.next
+
+        s3 = s1 + s2
+        tail = None
+        head = None
+        while s3 > 0:
+            head = ListNode(s3 % 10)
+            head.next = tail
+            tail = head
+            s3 = s3 // 10
+        return head if head else ListNode(0)
