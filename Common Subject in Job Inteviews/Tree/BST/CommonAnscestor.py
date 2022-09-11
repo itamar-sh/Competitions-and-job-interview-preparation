ad108240e263423ff47d17f999c9643d0a840a09
Itamar Shechter <84578656+itamar-sh@users.noreply.github.com>
2022-09-11 18:49:58 +0300

    Lowest Common Ancestor of a Binary Search Tree - Medium - 235

---
+++ Common Subject in Job Inteviews/Tree/BST/CommonAnscestor.py
@@ -0,0 +1,18 @@
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        """
+        Given BST and two nodes  - find their lowest Common Ancestor.
+        """
+        while root:
+            if root.val < p.val and root.val < q.val:
+                root = root.right
+            elif root.val > p.val and root.val > q.val:
+                root = root.left
+            else:
+                return root
