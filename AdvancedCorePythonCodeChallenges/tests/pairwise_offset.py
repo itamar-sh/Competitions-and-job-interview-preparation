cae663681c110c8f3c9fbbcfa7d5c9768e8b7524
Itamar Shechter <84578656+itamar-sh@users.noreply.github.com>
2022-09-16 17:48:31 +0300

    my solution

--- AdvancedCorePythonCodeChallenges/tests/pairwise_offset.py
+++ AdvancedCorePythonCodeChallenges/tests/pairwise_offset.py
@@ -6,10 +6,15 @@ T = TypeVar('T')      # Declare type variable
 def pairwise_offset(sequence: Sequence[T], fillvalue="*", offset=0) -> Sequence[T]:
     seq = []
     for i in range(offset):
-        seq.append((sequence[i], fillvalue))
+        seq.append((sequence[i], fillvalue) if i < len(sequence) else (fillvalue, fillvalue))
     for i in range(len(sequence)):
         seq.append((sequence[i+offset], sequence[i]) if i + offset < len(sequence) else (fillvalue, sequence[i]))
     return seq
 
-print(pairwise_offset(['a', 'b', 'c'], offset=1))
-print(pairwise_offset(['a', 'b', 'c'], offset=2))
+if __name__ == '__main__':
+
+    print(pairwise_offset(['a', 'b', 'c'], offset=1))
+    print(pairwise_offset(['a', 'b', 'c'], offset=2))
+    actual = list(pairwise_offset('abcde'))
+    expected = [('a', 'a'), ('b', 'b'), ('c', 'c'), ('d', 'd'), ('e', 'e')]
+    assert expected == actual
